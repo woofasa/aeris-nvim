34b04947ad6b04aa8306fb639522468834b8df3b
woofasa <woofasa132@gmail.com>
2026-06-15 00:32:09 +0300

    Lsp updated: diagnostic things added

--- lua/aeris/groups/lsp.lua
+++ lua/aeris/groups/lsp.lua
@@ -2,7 +2,22 @@ local lsp = {}
 
 lsp.load = function(aeris)
   return {
-    LspInlayHint = { fg = aeris.fg_subtle, bg = aeris.bg_raised },
+    LspInlayHint               = { fg = aeris.fg_subtle, bg = aeris.bg_raised },
+
+    DiagnosticVirtualTextError = { fg = aeris.red, bg = aeris.bg_raised },
+    DiagnosticVirtualTextWarn  = { fg = aeris.peach, bg = aeris.bg_raised },
+    DiagnosticVirtualTextInfo  = { fg = aeris.aqua, bg = aeris.bg_raised },
+    DiagnosticVirtualTextHint  = { fg = aeris.green, bg = aeris.bg_raised },
+
+    DiagnosticError            = { fg = aeris.red },
+    DiagnosticWarn             = { fg = aeris.peach },
+    DiagnosticInfo             = { fg = aeris.aqua },
+    DiagnosticHint             = { fg = aeris.green },
+
+    DiagnosticUnderlineError   = { undercurl = true, sp = aeris.red },
+    DiagnosticUnderlineWarn    = { undercurl = true, sp = aeris.peach },
+    DiagnosticUnderlineInfo    = { undercurl = true, sp = aeris.aqua },
+    DiagnosticUnderlineHint    = { undercurl = true, sp = aeris.green },
   }
 end
 
